7065a71b5c965d177684c0b8eb79fb5ec086c42f
John Corredor, PhD <82288780+corredor-john@users.noreply.github.com>
2023-05-16 11:24:19 -0500

    Delete RutaFONTIC directory

--- RutaFONTIC/JavaScript/projec01/scripts/script01.js
+++
@@ -1,5 +0,0 @@
-/* Autor: John Corredor
-   Fecha: 23-nov-2021
-*/
-
-alert('Hola mundo!: desde un Script EXTERNO'); 
--- RutaFONTIC/JavaScript/projec01/scripts/script04.js
+++
@@ -1,9 +0,0 @@
-function multiplicacion(num1,num2){
-	if (num1 == 2) {
-		return (num1*num2);
-	} else {
-		return (num1+2);
-	}
-}
-
-alert("Resultado de la funcion multiplicar: "+ multiplicacion(3,3));
